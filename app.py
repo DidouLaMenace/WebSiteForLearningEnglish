40184f4e686edc0eb5e2b34c3979f817fab9d8ef
Dylan Fournier <dfournier1305@gmail.com>
2023-02-07 11:39:32 +0100

    C'est un peu plus jolie

--- app.py
+++ app.py
@@ -2,35 +2,33 @@ from flask import Flask,render_template,request
 
 app=Flask(__name__)
 
-##### TOURNOI #####
 @app.route("/")
 def page_accueil():
-    return render_template("tournoi.html")
+    return render_template("accueil/english.html")
 
-### Exercice ###
-@app.route("/culture")
+@app.route("/culture&history")
 def edit2021():
-    return render_template("culture.html")
+    return render_template("cult/culture.html")
 
 @app.route("/grammar")
 def edit2022():
-    return render_template("grammar.html")
+    return render_template("gram/grammar.html")
 
 @app.route("/vocabulary")
 def editsuivante():
-    return render_template("vocabulary.html")
+    return render_template("voc/vocabulary.html")
 
 @app.route("/card-voc-theme")
 def card_voc_theme():
-    return render_template("stack-voc.html")
+    return render_template("voc/stack-voc.html")
 
 @app.route("/card-gram-theme")
 def card_gram_theme():
-    return render_template("stack-gram.html")
+    return render_template("gram/stack-gram.html")
 
 @app.route("/card-culture-theme")
 def card_cult_theme():
-    return render_template("stack-cult.html")
+    return render_template("cult/stack-cult.html")
 
 if __name__=="__main__":
     app.debug=True
