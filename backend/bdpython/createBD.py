7644800d7d2699e9498b428447126614b5b0177e
Dylan Fournier <dfournier1305@gmail.com>
2023-03-13 13:28:58 +0100

    pour etre sur

--- backend/bdpython/createBD.py
+++ backend/bdpython/createBD.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-dico = open("/home/didoulamenace/Documents/Telecom/Anglais/anglais/backend/bd/toeic.txt",'r')
+dico = open("/home/didoulamenace/Documents/Telecom/Anglais/anglais/backend/bd/sports.txt",'r')
 
 con = sqlite3.connect("/home/didoulamenace/Documents/Telecom/Anglais/anglais/backend/bd/question.db")
 cur = con.cursor()
@@ -9,14 +9,14 @@ liste = dico.readlines()
 
 a=1502
 
-id_field = 1
-sujet = "Special TOEIC"
+id_field = 3
+sujet = "Sports and Leisure"
 
 for i in range(0,len(liste),2):
     question=liste[i]
     answer=liste[i+1]
-    query = "INSERT INTO card VALUES (?,?,?,?,?);"
-    ajout=(a,sujet,question,answer,id_field)
+    query = "INSERT INTO card (sujet,question,answer,id_field) VALUES (?,?,?,?);"
+    ajout=(sujet,question,answer,id_field)
     cur.execute(query,ajout)
     a+=1
 
